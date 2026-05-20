78cf165d50d49f95bfbb56f1c1c597a05db4495d
pedrocosta <pedrohenriquecs012@gmail.com>
2026-05-20 11:55:20 -0300

    repo clonado

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bem vindo ao curso de git e gitHub
+
+DESCRIÇÃO DO MEU README
---
+++ login.js
@@ -0,0 +1,10 @@
+// Trabalhando no login
+
+// Finalizando sistema de login
+
+function login (nome, email){
+    return 'Bem Vindo!'
+}
+
+// Seja bem vindo
+// Retornamos o nome do usuário
